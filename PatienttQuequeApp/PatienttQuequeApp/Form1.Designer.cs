c32875ec7a55b783a97c904867680e28aefc5a89
Miftahunmohin <miftahunmohin@yahoo.com>
2014-09-28 14:39:10 +0600

    Added

---
+++ PatienttQuequeApp/PatienttQuequeApp/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace PatienttQuequeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.AddresstextBox = new System.Windows.Forms.TextBox();
+            this.AgetextBox = new System.Windows.Forms.TextBox();
+            this.NametextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.AddressshowtextBox = new System.Windows.Forms.TextBox();
+            this.AgeshowtextBox = new System.Windows.Forms.TextBox();
+            this.NameshowtextBox = new System.Windows.Forms.TextBox();
+            this.SerialtextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Serialcolum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Namecolumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Agecolum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Adresscolumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Enqueuebutton = new System.Windows.Forms.Button();
+            this.Dequeuebutton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Enqueuebutton);
+            this.groupBox1.Controls.Add(this.AddresstextBox);
+            this.groupBox1.Controls.Add(this.AgetextBox);
+            this.groupBox1.Controls.Add(this.NametextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(359, 260);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // AddresstextBox
+            // 
+            this.AddresstextBox.Location = new System.Drawing.Point(212, 169);
+            this.AddresstextBox.Name = "AddresstextBox";
+            this.AddresstextBox.Size = new System.Drawing.Size(100, 20);
+            this.AddresstextBox.TabIndex = 5;
+            // 
+            // AgetextBox
+            // 
+            this.AgetextBox.Location = new System.Drawing.Point(212, 98);
+            this.AgetextBox.Name = "AgetextBox";
+            this.AgetextBox.Size = new System.Drawing.Size(100, 20);
+            this.AgetextBox.TabIndex = 4;
+            // 
+            // NametextBox
+            // 
+            this.NametextBox.Location = new System.Drawing.Point(212, 26);
+            this.NametextBox.Name = "NametextBox";
+            this.NametextBox.Size = new System.Drawing.Size(100, 20);
+            this.NametextBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Age";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Dequeuebutton);
+            this.groupBox2.Controls.Add(this.AddressshowtextBox);
+            this.groupBox2.Controls.Add(this.AgeshowtextBox);
+            this.groupBox2.Controls.Add(this.NameshowtextBox);
+            this.groupBox2.Controls.Add(this.SerialtextBox);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(377, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(381, 288);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // AddressshowtextBox
+            // 
+            this.AddressshowtextBox.Location = new System.Drawing.Point(169, 210);
+            this.AddressshowtextBox.Name = "AddressshowtextBox";
+            this.AddressshowtextBox.Size = new System.Drawing.Size(100, 20);
+            this.AddressshowtextBox.TabIndex = 7;
+            // 
+            // AgeshowtextBox
+            // 
+            this.AgeshowtextBox.Location = new System.Drawing.Point(169, 142);
+            this.AgeshowtextBox.Name = "AgeshowtextBox";
+            this.AgeshowtextBox.Size = new System.Drawing.Size(100, 20);
+            this.AgeshowtextBox.TabIndex = 6;
+            // 
+            // NameshowtextBox
+            // 
+            this.NameshowtextBox.Location = new System.Drawing.Point(169, 81);
+            this.NameshowtextBox.Name = "NameshowtextBox";
+            this.NameshowtextBox.Size = new System.Drawing.Size(100, 20);
+            this.NameshowtextBox.TabIndex = 5;
+            // 
+            // SerialtextBox
+            // 
+            this.SerialtextBox.Location = new System.Drawing.Point(169, 23);
+            this.SerialtextBox.Name = "SerialtextBox";
+            this.SerialtextBox.Size = new System.Drawing.Size(100, 20);
+            this.SerialtextBox.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 210);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Age";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Serial";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Serialcolum,
+            this.Namecolumn,
+            this.Agecolum,
+            this.Adresscolumn});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 306);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(718, 197);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Serialcolum
+            // 
+            this.Serialcolum.Text = "Serial";
+            // 
+            // Namecolumn
+            // 
+            this.Namecolumn.Text = "Name";
+            // 
+            // Agecolum
+            // 
+            this.Agecolum.Text = "Age";
+            // 
+            // Adresscolumn
+            // 
+            this.Adresscolumn.Text = "Adress";
+            // 
+            // Enqueuebutton
+            // 
+            this.Enqueuebutton.Location = new System.Drawing.Point(212, 231);
+            this.Enqueuebutton.Name = "Enqueuebutton";
+            this.Enqueuebutton.Size = new System.Drawing.Size(75, 23);
+            this.Enqueuebutton.TabIndex = 6;
+            this.Enqueuebutton.Text = "Enqueue";
+            this.Enqueuebutton.UseVisualStyleBackColor = true;
+            this.Enqueuebutton.Click += new System.EventHandler(this.Enqueuebutton_Click);
+            // 
+            // Dequeuebutton
+            // 
+            this.Dequeuebutton.Location = new System.Drawing.Point(169, 259);
+            this.Dequeuebutton.Name = "Dequeuebutton";
+            this.Dequeuebutton.Size = new System.Drawing.Size(75, 23);
+            this.Dequeuebutton.TabIndex = 8;
+            this.Dequeuebutton.Text = "Dequeue";
+            this.Dequeuebutton.UseVisualStyleBackColor = true;
+            this.Dequeuebutton.Click += new System.EventHandler(this.Dequeuebutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 504);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox AddresstextBox;
+        private System.Windows.Forms.TextBox AgetextBox;
+        private System.Windows.Forms.TextBox NametextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox AddressshowtextBox;
+        private System.Windows.Forms.TextBox AgeshowtextBox;
+        private System.Windows.Forms.TextBox NameshowtextBox;
+        private System.Windows.Forms.TextBox SerialtextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button Enqueuebutton;
+        private System.Windows.Forms.Button Dequeuebutton;
+        private System.Windows.Forms.ColumnHeader Serialcolum;
+        private System.Windows.Forms.ColumnHeader Namecolumn;
+        private System.Windows.Forms.ColumnHeader Agecolum;
+        private System.Windows.Forms.ColumnHeader Adresscolumn;
+    }
+}
+
---
+++ PatienttQuequeApp/PatienttQuequeApp/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PatienttQuequeApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Patient aPatient = new Patient();
+        Queue<Patient> aQueue = new Queue<Patient>();
+        private int serial =1;
+        private void Enqueuebutton_Click(object sender, EventArgs e)
+        {
+
+            if (NametextBox.Text=="" ||AgetextBox.Text=="" )
+            {
+                MessageBox.Show("Pls Entry Name");
+            }
+
+            aPatient.serial = serial.ToString();
+            aPatient.name = NametextBox.Text;
+            aPatient.age = Convert.ToInt32(AgetextBox.Text);
+            aPatient.address = AddresstextBox.Text;
+            aQueue.Enqueue(aPatient);
+            serial++;
+            ListViewItem item = new ListViewItem();
+            item.Text = aPatient.serial;
+            item.SubItems.Add(aPatient.name);
+            item.SubItems.Add(aPatient.age.ToString());
+            item.SubItems.Add(aPatient.address);
+            listView1.Items.Add(item);
+
+            NametextBox.Text = string.Empty;
+            AgetextBox.Text = string.Empty;
+            AddresstextBox.Text = string.Empty;
+
+
+        }
+
+        private void Dequeuebutton_Click(object sender, EventArgs e)
+        {
+            aPatient = aQueue.Dequeue();
+            SerialtextBox.Text = aPatient.serial;
+            NameshowtextBox.Text = aPatient.name;
+            AgeshowtextBox.Text = aPatient.age.ToString();
+            AddressshowtextBox.Text = aPatient.address;
+            listView1.Items[0].Remove();
+        }
+
+       
+    }
+}
